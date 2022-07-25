fea7aa90c2387f09509f7e5f7509bf5918bdae0b
nenomius <nenomius@protonmail.com>
2022-07-25 20:12:51 +0300

    Autocompleter rework
    Trie of words and full texts replaced by suffix array of full text:
    - capable of matching in the middle of a word;
    - consumes less memory on big datasets;
    - builds faster;
    - matches are ranked according to edit distance and position in text.

--- src/CompletionProxyModel.cpp
+++ src/CompletionProxyModel.cpp
@@ -12,6 +12,358 @@
 #include "Logging.h"
 #include "Utils.h"
 
+#include "libsais16.h"
+
+#include <ranges>
+#include <span>
+
+struct CompletionProxyModel::Index
+{
+    // concatenated text of all searchable items
+    // delimited by '\0' to exclude matches across item boundaries
+    QString text;
+
+    // bitmask indicating positions of word aboundaries according to QTextBoundaryFinder
+    std::vector<bool> word_boundaries; // text.size + 1
+
+    // positions in concatenated text where each item starts, plus size of whole text
+    // text of item i is in half-open range [item_starts[i], item_starts[i+1])
+    std::vector<int> item_starts; // item count + 1
+
+    // suffix array of concatenated text and its inverse
+    std::vector<int> SA;    // text.size
+    std::vector<int> invSA; // text.size
+
+    struct index_range
+    {
+        std::size_t lo = 0;
+        std::size_t hi = 0;
+
+        auto begin() const noexcept { return std::ranges::iota_view(lo, hi).begin(); }
+        auto end() const noexcept { return std::ranges::iota_view(lo, hi).end(); }
+        bool empty() const { return lo == hi; }
+    };
+
+    struct prefix_range : index_range
+    {
+        int length = 0;
+    };
+
+    struct match_range : prefix_range
+    {
+        int edits = 0;
+    };
+
+    Index(QString &&text_, std::vector<int> &&item_starts_)
+      : text(std::move(text_))
+      , item_starts(std::move(item_starts_))
+    {
+        assert(item_starts.front() == 0);
+        item_starts.push_back(text.size());
+
+        text.shrink_to_fit();
+        item_starts.shrink_to_fit();
+
+        SA.resize(text.size());
+        if (auto ec = libsais16(text.utf16(), SA.data(), text.size(), 0, nullptr); ec != 0) {
+            nhlog::ui()->error("Failed to build autocomplete index! No completions for you!");
+
+            // reset to empty index
+            text.clear();
+            word_boundaries = {false};
+            item_starts = {0};
+            SA.clear();
+            invSA.clear();
+
+            return;
+        }
+
+        invSA.resize(text.size());
+        for (auto i : index_range{0, SA.size()}) {
+            invSA[SA[i]] = i;
+        }
+
+        word_boundaries.resize(text.size() + 1);
+        auto bounds = QTextBoundaryFinder(QTextBoundaryFinder::BoundaryType::Word, text);
+        bounds.toStart();
+        do {
+            word_boundaries[bounds.position()] = true;
+        } while (bounds.toNextBoundary() != -1);
+    }
+
+    int item_at(int text_pos) const noexcept
+    {
+        assert(not item_starts.empty());
+        assert(text_pos < text.size());
+        return std::ranges::lower_bound(item_starts, text_pos, std::less_equal{}) -
+               item_starts.begin() - 1;
+    }
+
+    prefix_range empty_prefix() const noexcept { return {{.lo = 0, .hi = SA.size()}, 0}; }
+
+    prefix_range extend_prefix(prefix_range p, QChar c) const noexcept
+    {
+        auto next_char = [this, k = p.length](std::size_t i) { return text[SA[i] + k]; };
+
+        p.lo = *std::ranges::lower_bound(p, c, std::less{}, next_char);
+        p.hi = *std::ranges::upper_bound(p, c, std::less{}, next_char);
+        p.length += 1;
+
+        return p;
+    }
+
+    prefix_range extend_prefix(prefix_range p, uint c) const noexcept
+    {
+        if (not QChar::requiresSurrogates(c)) {
+            return extend_prefix(p, QChar(c));
+        } else {
+            p = extend_prefix(p, QChar::highSurrogate(c));
+            return extend_prefix(p, QChar::lowSurrogate(c));
+        }
+    }
+
+    prefix_range extend_prefix(prefix_range p, std::span<const uint> str) const noexcept
+    {
+        for (auto c : str) {
+            p = extend_prefix(p, c);
+        }
+
+        return p;
+    }
+
+    prefix_range range_of(std::span<const uint> str) const noexcept
+    {
+        return extend_prefix(empty_prefix(), str);
+    }
+
+    prefix_range concat_prefix(prefix_range p1, prefix_range p2) const noexcept
+    {
+        auto pred = [this, k = p1.length](int x) { return invSA[SA[x] + k]; };
+        p1.lo     = *std::ranges::lower_bound(p1, p2.lo, std::less{}, pred);
+        p1.hi     = *std::ranges::upper_bound(p1, p2.hi - 1, std::less{}, pred);
+        p1.length += p2.length;
+        return p1;
+    };
+
+    std::pair<prefix_range, QChar> first_subrange(prefix_range p) const noexcept
+    {
+        auto next_char = [this, k = p.length](std::size_t i) { return text[SA[i] + k]; };
+
+        // skip null character range if present
+        p.lo = *std::ranges::upper_bound(p, QChar(), std::less{}, next_char);
+
+        if (p.empty()) {
+            return {p, QChar()};
+        }
+
+        const auto c = next_char(p.lo);
+        p.hi         = *std::ranges::upper_bound(p, c, std::less{}, next_char);
+        p.length += 1;
+
+        return {p, c};
+    }
+
+    std::pair<prefix_range, QChar> next_subrange(prefix_range p, prefix_range s) const noexcept
+    {
+        p.lo = s.hi;
+        return first_subrange(p);
+    }
+
+    std::pair<prefix_range, uint> first_subrange_utf32(prefix_range p) const noexcept
+    {
+        auto [s, c] = first_subrange(p);
+
+        if (c.isHighSurrogate()) {
+            auto [s2, c2] = first_subrange(s);
+            return {s2, QChar::surrogateToUcs4(c, c2)};
+        }
+
+        return {s, c.unicode()};
+    }
+
+    std::pair<prefix_range, uint> next_subrange_utf32(prefix_range p, prefix_range s) const noexcept
+    {
+        p.lo = s.hi;
+        return first_subrange_utf32(p);
+    }
+
+    template<typename F>
+    void for_each_succesor_of(prefix_range p, const F &f)
+    {
+        auto [s, c] = first_subrange_utf32(p);
+        while (s.lo != p.hi) {
+            f(s, c);
+            std::tie(s, c) = next_subrange_utf32(p, s);
+        }
+    }
+
+    std::vector<match_range> search(QStringView pattern, std::size_t max_edits)
+    {
+        return search(pattern.toUcs4(), max_edits);
+    }
+
+    std::vector<match_range> search(std::span<const uint> pattern, int max_edits)
+    {
+        // Approximate String Matching Using Compressed Suffix Arrays
+        // DOI: 10.1007/978-3-540-27801-6_33
+
+        auto pattern_suffixes = std::vector<prefix_range>(pattern.size() + 1);
+        for (auto i : index_range{0, pattern.size()}) {
+            pattern_suffixes[i] = range_of(pattern.subspan(i));
+        }
+
+        auto results = std::vector<match_range>{};
+
+        enum class edit_op
+        {
+            noop,
+            ins,
+            del,
+            rep,
+            swap,
+        };
+
+        auto kapproximate =
+          [&](auto self, prefix_range p, std::size_t i, int edits, edit_op last_op) {
+              if (auto r = concat_prefix(p, pattern_suffixes[i]); not r.empty()) {
+                  results.push_back({r, edits});
+              }
+
+              if (p.empty() or edits >= max_edits) {
+                  return;
+              }
+
+              for (auto j = i; j < pattern.size() and not p.empty(); ++j) {
+                  // deletion at j
+                  // dont do del-ins, or ins-del since that pair is same as replace, but in 2 edits
+                  if (last_op != edit_op::ins) {
+                      self(self, p, j + 1, edits + 1, edit_op::del);
+                  }
+
+                  // swap at j with j+1
+                  if (j + 1 < pattern.size()) {
+                      auto p_next = extend_prefix(p, pattern[j + 1]);
+                      p_next      = extend_prefix(p_next, pattern[j]);
+                      self(self, p_next, j + 2, edits + 1, edit_op::swap);
+                  }
+
+                  for_each_succesor_of(p, [&](prefix_range s, uint c) {
+                      if (c == pattern[j]) {
+                          return;
+                      }
+
+                      // replacemen at j
+                      self(self, s, j + 1, edits + 1, edit_op::rep);
+
+                      // insertion at j
+                      if (last_op != edit_op::del) {
+                          self(self, s, j, edits + 1, edit_op::ins);
+                      }
+                  });
+
+                  // step unedited
+                  p = extend_prefix(p, pattern[j]);
+              }
+
+              // try also insertions at pattern end
+              if (last_op != edit_op::del) {
+                  auto j = pattern.size();
+                  for_each_succesor_of(p, [&](prefix_range s, uint c) {
+                      std::ignore = c;
+
+                      // insertion at j
+                      self(self, s, j, edits + 1, edit_op::ins);
+                  });
+              }
+          };
+
+        kapproximate(kapproximate, empty_prefix(), 0, 0, edit_op::noop);
+
+        return results;
+    }
+
+    float match_weight(int item, QStringView item_text, int edits, int match_start, int match_end)
+    {
+        // TODO add static item weights to mix in ranking function
+
+        const auto text_size      = item_text.size();
+        const auto secondary_text = item_text.indexOf(QChar());
+
+        const bool is_exact       = (edits == 0);
+        const bool is_full_prefix = (match_start == 0 or item_text[match_start - 1].isNull());
+        const bool is_full_suffix = (match_end == text_size or item_text[match_end].isNull());
+        const bool is_full_match  = is_full_prefix and is_full_suffix;
+        const bool is_word_prefix = word_boundaries[item_starts[item] + match_start];
+        const bool is_word_suffix = word_boundaries[item_starts[item] + match_end];
+        const bool is_word_match  = is_word_prefix and is_word_suffix;
+        const bool is_secondary   = (match_start > secondary_text);
+
+        const auto lexical_order =
+          (1.0f - static_cast<float>(invSA[item_starts[item]]) / text.size());
+
+        // WARNING i got these numbers by poking around in my ass
+        auto w = 0.0f;
+        w += 3.50f * is_exact;
+        w += 2.00f * is_full_prefix;
+        w += 1.00f * is_full_match;
+        w += 1.10f * (is_word_prefix + 0.5 * is_word_match);
+        w += 2.00f * (1.0f - static_cast<float>(match_start) / text_size);
+        w -= 1.00f * is_secondary;
+        w -= 1.50f * edits;
+        w += 0.10f * lexical_order;
+
+        return w;
+    }
+
+    std::vector<int> top_items(QStringView pattern, std::size_t max_distance, std::size_t max_items)
+    {
+        const auto found_ranges = search(pattern, max_distance);
+
+        std::vector<float> weights;
+        weights.resize(item_starts.size(), NAN);
+
+        std::vector<int> items;
+
+        std::size_t duplicates = 0;
+        for (auto r : found_ranges) {
+            for (auto prefix_id : r) {
+                const auto edit_distance  = r.edits;
+                const auto length         = r.length;
+                const auto item           = item_at(SA[prefix_id]);
+                const auto item_start_pos = item_starts[item];
+                const auto item_text_size = item_starts[item + 1] - item_start_pos;
+                const auto match_start    = SA[prefix_id] - item_start_pos;
+                const auto match_end      = match_start + length;
+                const auto item_text      = text.midRef(item_start_pos, item_text_size);
+
+                const auto weight =
+                  match_weight(item, item_text, edit_distance, match_start, match_end);
+
+                if (std::isnan(weights[item])) {
+                    // found this item first time, add to item list
+                    items.push_back(item);
+                    weights[item] = weight;
+                } else {
+                    ++duplicates;
+                    weights[item] = std::max(weights[item], weight);
+                }
+            }
+        }
+
+        auto item_weight = [&](int i) { return weights[i]; };
+
+        // get best max_items results
+        if (items.size() > max_items) {
+            std::ranges::nth_element(items, items.begin() + max_items, std::greater{}, item_weight);
+            items.resize(max_items);
+        }
+
+        std::ranges::sort(items, std::greater{}, item_weight);
+
+        return items;
+    }
+};
+
 CompletionProxyModel::CompletionProxyModel(QAbstractItemModel *model,
                                            int max_mistakes,
                                            size_t max_completions,
@@ -22,49 +374,33 @@ CompletionProxyModel::CompletionProxyModel(QAbstractItemModel *model,
 {
     setSourceModel(model);
 
-    auto insertParts = [this](const QString &str, int id) {
-        QTextBoundaryFinder finder(QTextBoundaryFinder::BoundaryType::Word, str);
-        finder.toStart();
-        do {
-            auto start = finder.position();
-            finder.toNextBoundary();
-            auto end = finder.position();
+    max_completions_ = std::numeric_limits<size_t>::max();
 
-            auto ref = str.midRef(start, end - start).trimmed();
-            if (!ref.isEmpty())
-                trie_.insert<ElementRank::second>(ref.toUcs4(), id);
-        } while (finder.position() < str.size());
-    };
+    const auto row_count = sourceModel()->rowCount();
 
-    const auto start_at = std::chrono::steady_clock::now();
+    QString text;
+    std::vector<int> item_starts;
+    item_starts.reserve(row_count);
 
-    // insert full texts and partial matches
-    for (int i = 0; i < sourceModel()->rowCount(); i++) {
-        // full texts are ranked first and partial matches second
-        // that way when searching full texts will be first in result list
-
+    for (int i = 0; i < row_count; i++) {
         auto string1 = sourceModel()
                          ->data(sourceModel()->index(i, 0), CompletionModel::SearchRole)
                          .toString()
                          .toLower();
-        if (!string1.isEmpty()) {
-            trie_.insert<ElementRank::first>(string1.toUcs4(), i);
-            insertParts(string1, i);
-        }
 
         auto string2 = sourceModel()
                          ->data(sourceModel()->index(i, 0), CompletionModel::SearchRole2)
                          .toString()
                          .toLower();
-        if (!string2.isEmpty()) {
-            trie_.insert<ElementRank::first>(string2.toUcs4(), i);
-            insertParts(string2, i);
-        }
+
+        item_starts.push_back(text.size());
+        text.append(string1);
+        text.push_back('\0');
+        text.append(string2);
+        text.push_back('\0');
     }
 
-    const auto end_at     = std::chrono::steady_clock::now();
-    const auto build_time = std::chrono::duration<double, std::milli>(end_at - start_at);
-    nhlog::ui()->debug("CompletionProxyModel: build trie: {} ms", build_time.count());
+    index_.reset(new Index(std::move(text), std::move(item_starts)));
 
     // initialize default mapping
     mapping.resize(std::min(max_completions_, static_cast<size_t>(model->rowCount())));
@@ -75,19 +411,27 @@ CompletionProxyModel::CompletionProxyModel(QAbstractItemModel *model,
       &CompletionProxyModel::newSearchString,
       this,
       [this](QString s) {
+          if (not s.isEmpty() and QStringLiteral("#@~:").contains(s.front())) {
+              s = s.mid(1);
+          }
           searchString_ = s.toLower();
           invalidate();
       },
       Qt::QueuedConnection);
 }
 
+CompletionProxyModel::~CompletionProxyModel() = default;
+
 void
 CompletionProxyModel::invalidate()
 {
-    auto key = searchString_.toUcs4();
+    const auto max_edits = std::clamp(searchString_.length() / 3, 0, maxMistakes_);
+
     beginResetModel();
-    if (!key.empty()) // return default model data, if no search string
-        mapping = trie_.search(key, max_completions_, maxMistakes_);
+    // return default model data, if no search string
+    if (not searchString_.isEmpty()) {
+        mapping = index_->top_items(searchString_, max_edits, max_completions_);
+    }
     endResetModel();
 }
 
--- src/CompletionProxyModel.h
+++ src/CompletionProxyModel.h
@@ -9,153 +9,6 @@
 
 #include <QAbstractProxyModel>
 
-enum class ElementRank
-{
-    first,
-    second
-};
-
-template<typename Key, typename Value>
-struct trie
-{
-    std::vector<Value> values;
-    std::map<Key, trie> next;
-
-    template<ElementRank r>
-    void insert(const QVector<Key> &keys, const Value &v)
-    {
-        auto t = this;
-        for (const auto k : keys) {
-            t = &t->next[k];
-        }
-
-        if constexpr (r == ElementRank::first) {
-            t->values.insert(t->values.begin(), v);
-        } else if constexpr (r == ElementRank::second) {
-            t->values.push_back(v);
-        }
-    }
-
-    std::vector<Value> valuesAndSubvalues(size_t limit = -1) const
-    {
-        std::vector<Value> ret;
-        if (limit < 200)
-            ret.reserve(limit);
-
-        for (const auto &v : values) {
-            if (ret.size() >= limit)
-                return ret;
-            else
-                ret.push_back(v);
-        }
-
-        for (const auto &[k, t] : next) {
-            (void)k;
-            if (ret.size() >= limit)
-                return ret;
-            else {
-                auto temp = t.valuesAndSubvalues(limit - ret.size());
-                for (auto &&v : temp) {
-                    if (ret.size() >= limit)
-                        return ret;
-
-                    if (std::find(ret.begin(), ret.end(), v) == ret.end()) {
-                        ret.push_back(std::move(v));
-                    }
-                }
-            }
-        }
-
-        return ret;
-    }
-
-    std::vector<Value> search(const QVector<Key> &keys, //< TODO(Nico): replace this with a span
-                              size_t result_count_limit,
-                              size_t max_edit_distance_ = 2) const
-    {
-        std::vector<Value> ret;
-        if (!result_count_limit)
-            return ret;
-
-        if (keys.isEmpty())
-            return valuesAndSubvalues(result_count_limit);
-
-        auto append = [&ret, result_count_limit](std::vector<Value> &&in) {
-            for (auto &&v : in) {
-                if (ret.size() >= result_count_limit)
-                    return;
-
-                if (std::find(ret.begin(), ret.end(), v) == ret.end()) {
-                    ret.push_back(std::move(v));
-                }
-            }
-        };
-
-        auto limit = [&ret, result_count_limit] {
-            return std::min(result_count_limit, (result_count_limit - ret.size()) * 2);
-        };
-
-        // Try first exact matches, then with maximum errors
-        for (size_t max_edit_distance = 0;
-             max_edit_distance <= max_edit_distance_ && ret.size() < result_count_limit;
-             max_edit_distance += 1) {
-            if (max_edit_distance && ret.size() < result_count_limit) {
-                max_edit_distance -= 1;
-
-                // swap chars case
-                if (keys.size() >= 2) {
-                    auto t = this;
-                    for (int i = 1; i >= 0; i--) {
-                        if (auto e = t->next.find(keys[i]); e != t->next.end()) {
-                            t = &e->second;
-                        } else {
-                            t = nullptr;
-                            break;
-                        }
-                    }
-
-                    if (t) {
-                        append(t->search(keys.mid(2), limit(), max_edit_distance));
-                    }
-                }
-
-                // insert case
-                for (const auto &[k, t] : this->next) {
-                    if (k == keys[0])
-                        continue;
-                    if (ret.size() >= limit())
-                        break;
-
-                    // insert
-                    append(t.search(keys, limit(), max_edit_distance));
-                }
-
-                // delete character case
-                append(this->search(keys.mid(1), limit(), max_edit_distance));
-
-                // substitute case
-                for (const auto &[k, t] : this->next) {
-                    if (k == keys[0])
-                        continue;
-                    if (ret.size() >= limit())
-                        break;
-
-                    // substitute
-                    append(t.search(keys.mid(1), limit(), max_edit_distance));
-                }
-
-                max_edit_distance += 1;
-            }
-
-            if (auto e = this->next.find(keys[0]); e != this->next.end()) {
-                append(e->second.search(keys.mid(1), limit(), max_edit_distance));
-            }
-        }
-
-        return ret;
-    }
-};
-
 class CompletionProxyModel : public QAbstractProxyModel
 {
     Q_OBJECT
@@ -165,6 +18,7 @@ public:
                          int max_mistakes       = 2,
                          size_t max_completions = 30,
                          QObject *parent        = nullptr);
+    ~CompletionProxyModel();
 
     void invalidate();
 
@@ -189,9 +43,12 @@ signals:
     void newSearchString(QString);
 
 private:
+    struct Index;
+
     QString searchString_;
-    trie<uint, int> trie_;
+    QScopedPointer<Index> index_;
     std::vector<int> mapping;
     int maxMistakes_;
     size_t max_completions_;
 };
+
